a626586d59c5d20861e78e6b2b6007254bc347cc
mr-Vasyl <gerchak9999@gmail.com>
2020-12-09 22:39:52 +0200

    Updating

--- Lexical_structure/2.4.1_words.js
+++ Lexical_structure/2.4.1_words.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+/* Reserved Words:
+as    const    export   get        null   target void
+async continue extends  if         of     this   while
+await debugger false    import     return throw  with
+break default  finally  in         set    true   yield
+case  delete   for      instanceof static try
+catch do       from     let        super  typeof
+class else     function new        switch var
+*/
+console.table(["as const export get null target void async continue ]extends if of this while await debugger false import return throw with break default finally in set true yield case delete for instanceof static try catch do from let super typeof class else function new switch var"])
