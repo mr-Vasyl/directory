b0b5581d1d34e6eddfb1663891dcf7028201656b
mr-Vasyl <gerchak9999@gmail.com>
2020-12-09 23:01:27 +0200

    new chapter

--- Lexical_structure/2.4.1_words.js
+++ Lexical_structure/2.4.1_words.js
@@ -6,5 +6,6 @@ break default  finally  in         set    true   yield
 case  delete   for      instanceof static try
 catch do       from     let        super  typeof
 class else     function new        switch var
+
+enum imp l ements inter face pac kage private protected public arguments eval
 */
-console.table(["as const export get null target void async continue ]extends if of this while await debugger false import return throw with break default finally in set true yield case delete for instanceof static try catch do from let super typeof class else function new switch var"])
