73f28cea37345b42b0cb6ea79ad5c8a99e7c90f7
mr-Vasyl <gerchak9999@gmail.com>
2020-12-13 22:03:04 +0200

    new section

--- 3.Types_Values_Variables/3.4.boolen_null_symbol_Destructuring.js
+++ 3.Types_Values_Variables/3.4.boolen_null_symbol_Destructuring.js
@@ -43,3 +43,22 @@ let [x,y] = [1,2]; // Same as let x=1, y=2
 [x,y] = [x+1,y+1]; // Same as x = x + 1, y = y + 1
 [x,y] = [y,x]; // Swap the value of the two variables
 [x,y] // => [3,2]: the incremented and swapped values
+
+let o = { x: 1, y: 2 }; // The object we'll loop over
+for(const [name, value] of Object.entries(o)) {
+console.log(name, value); // Prints "x 1" and "y 2"
+}
+let [x,y] = [1]; // x == 1; y == undefined
+[x,y] = [1,2,3]; // x == 1; y == 2
+[,x,,y] = [1,2,3,4]; // x == 2; y == 4
+
+let [x, ...y] = [1,2,3,4]; // y == [2,3,4]
+let [a, [b, c]] = [1, [2,2.5], 3]; // a == 1; b == 2; c == 2.5
+let [first, ...rest] = "Hello"; // first == "H"; rest == ["e","l","l","o"]
+
+let transparent = {r: 0.0, g: 0.0, b: 0.0, a: 1.0}; // A RGBA color
+let {r, g, b} = transparent; // r == 0.0; g == 0.0; b == 0.0
+
+// Same as const sin=Math.sin, cos=Math.cos, tan=Math.tan
+const {sin, cos, tan} = Math;
+
