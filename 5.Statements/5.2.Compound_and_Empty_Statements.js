e2365cd362998b2a27b01080bee82a0c20a49340
mr-Vasyl <gerchak9999@gmail.com>
2020-12-20 17:15:21 +0200

    add while, for, for/of/in

---
+++ 5.Statements/5.2.Compound_and_Empty_Statements.js
@@ -0,0 +1,84 @@
+// косинус від числа
+{
+	x = Math.PI;
+	cx = Math.cos(x);
+	console.log('cos(π) = ' + cx);
+}
+// Если имя пользователя null, undefined, false, О, " " или NaN,
+// то присвоить ему новое значение
+let username = null;
+if (!username) username = 'Marcus';
+console.log(username);
+
+function convert(x) {
+	switch (typeof x) {
+		case 'number':
+			return x.toString(16);
+		case 'string':
+			return '"' + x + '"';
+		default:
+			return String(x);
+	}
+}
+console.log(convert(123));
+
+//while
+let count = 0;
+while (count < 10) {
+	console.log(count);
+	count++;
+}
+
+//do-while
+let arr = [ 1, 4, 65, 5 ];
+function printArr(a) {
+	let len = a.length,
+		i = 0;
+	if (len === 0) {
+		console.log('пустий масив');
+	} else {
+		do {
+			console.log(a[i]);
+		} while (++i < len);
+	}
+}
+printArr(arr);
+
+//for
+for (let count = 20; count > 0; count--) {
+	console.log(count + ' по порядку');
+}
+let i,
+	j,
+	sum = 0;
+for (i = 0, j = 10; i < 10; i++, j--) {
+	sum += i * j;
+	console.log(i, j, sum);
+}
+let arry = [ 1, 2, 3, 4, 5 ],
+	sum = 0,
+	data;
+for (data of arry) {
+	sum = sum + data;
+}
+console.log(sum);
+// for in with object
+let obj = { a: 1, b: 2 };
+for (let count in obj) {
+	console.log(`obj - ${count}:${obj[count]}`);
+}
+// по ключам
+for (let o of Object.keys(obj)) {
+	console.log(o);
+}
+// по значенням
+let sum = 0;
+for (let a of Object.values(obj)) {
+	sum += a;
+}
+console.log(sum);
+//ключі і значення
+for (let [key, val] of Object.entries(obj)) {
+	console.log(key,val);
+}
+
