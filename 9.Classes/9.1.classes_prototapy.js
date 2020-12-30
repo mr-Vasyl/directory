cd1915d6b552fbf3c3e6916b7c8c699fe291a0c1
mr-Vasyl <gerchak9999@gmail.com>
2020-12-30 23:05:22 +0200

    constructor

--- 9.Classes/9.1.classes_prototapy.js
+++ 9.Classes/9.1.classes_prototapy.js
@@ -2,17 +2,17 @@
 function range(from, to) {
 	let r = Object.create(range.methods);
 	r.from = from;
-    r.to = to;
-     console.log(r)
-    return r;
-   
+	r.to = to;
+	console.log(r);
+	return r;
 }
 
 range.methods = {
 	includes(x) {
 		return this.from <= x && x <= this.to;
 	},
-	*[Symbol.iterator]() { // робить обєкт ітеріруєм 
+	*[Symbol.iterator]() {
+		// робить обєкт ітеріруєм
 		for (let x = Math.ceil(this.from); x <= this.to; x++) yield x;
 	},
 	toString() {
@@ -27,17 +27,18 @@ console.log(r.toString()); // (1...3)
 console.log([ ...r ]); // [1,2,3] через ітератор в масив
 
 // класс конструктор
-// простой класс
+// з прототипом
 function Range(from, to) {
 	this.from = from;
-    this.to = to;  
+	this.to = to;
 }
 
 Range.prototype = {
 	includes: function(x) {
 		return this.from <= x && x <= this.to;
 	},
-	[Symbol.iterator]: function*() { // робить обєкт ітеріруєм 
+	[Symbol.iterator]: function*() {
+		// робить обєкт ітеріруєм
 		for (let x = Math.ceil(this.from); x <= this.to; x++) yield x;
 	},
 	toString: function() {
@@ -50,6 +51,28 @@ console.log(r2.includes(2)); // true 2 входить в діапазон
 console.log(r2.toString()); // (1...3)
 console.log([ ...r2 ]); // [1,2,3] через ітератор в масив
 
-// instanceof 
-console.log(r2 instanceof Range) // true
+// instanceof
+console.log(r2 instanceof Range); // true
 
+// клас
+class Range3 {
+	constructor(from, to) {
+		this.from = from;
+		this.to = to;
+	}
+	includes(x) {
+		return this.from <= x && x <= this.to;
+	}
+	*[Symbol.iterator]() {
+		// робить обєкт ітеріруєм
+		for (let x = Math.ceil(this.from); x <= this.to; x++) yield x;
+	}
+	toString() {
+		return '(' + this.from + '...' + this.to + ')';
+	}
+}
+
+let r3 = new Range3(1, 3);
+console.log(r3.includes(2)); // true 2 входить в діапазон
+console.log(r3.toString()); // (1...3)
+console.log([ ...r3 ]); // [1,2,3] через ітератор в масив
